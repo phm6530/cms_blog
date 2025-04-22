b12bd8512e89649afaead4e8a9f86be44e1043dc
hm <squirrel309@naver.com>
2025-04-22 19:07:17 +0900

    미들웨어 실행이 안됨으로 일단 서버컴포넌트로 대체

--- src/app/(public)/login/layout.tsx
+++ src/app/(public)/login/layout.tsx
@@ -1,12 +1,14 @@
 import { auth } from "@/auth";
 import { redirect } from "next/navigation";
+
 import { ReactNode } from "react";
 
 export default async function layout({ children }: { children: ReactNode }) {
   const session = await auth();
 
-  if (!session) {
-    redirect("/login");
+  if (session?.user) {
+    redirect("/");
   }
-  return <div className="mt-8">{children}</div>;
+
+  return <>{children}</>;
 }
