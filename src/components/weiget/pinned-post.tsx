0fb257f72413e30c8d3c944be98109941b39cbb8
hm <squirrel309@naver.com>
2025-10-07 11:19:45 +0900

    서버컴포넌트에서 api Router 호출 오버헤드 초래 때문에 r unstalbe_cache로 변경

--- src/components/weiget/pinned-post.tsx
+++ src/components/weiget/pinned-post.tsx
@@ -1,25 +1,16 @@
-import { REVALIDATE } from "@/type/constants";
-import { withFetchRevaildationAction } from "@/util/withFetchRevaildationAction";
 import { PinnedPostModel } from "@/type/post.type";
 import { notFound } from "next/navigation";
 import { CarouselSlide } from "../shared/carousel-slide";
 import { Pin } from "lucide-react";
+import getPinnedPosts from "@/service/pinned-post";
 
 export default async function PinnedPosts() {
-  const response = await withFetchRevaildationAction<Array<PinnedPostModel>>({
-    endPoint: `api/pinned`,
-    options: {
-      cache: "force-cache",
-      next: {
-        tags: [REVALIDATE.PINNED_POST],
-      },
-    },
-  });
+  const response = await getPinnedPosts<Array<PinnedPostModel>>();
 
-  if (!response.success) {
+  if (!response) {
     notFound();
   }
-  const data = response.result;
+  const data = response;
 
   return (
     <div className=" gap-1 relative w-full  grid grid-cols-[1fr_2fr] mt-20">
@@ -31,7 +22,7 @@ export default async function PinnedPosts() {
       {!data || data.length === 0 ? (
         "등록된 고정 콘텐츠가 없습니다."
       ) : (
-        <CarouselSlide postList={response.result} />
+        <CarouselSlide postList={response} />
       )}
     </div>
   );
---
+++ src/service/pinned-post.ts
@@ -0,0 +1,64 @@
+import { db } from "@/db/db";
+import { blogMetaSchema } from "@/db/schema/blog-metadata";
+import { blogSubGroup } from "@/db/schema/category";
+import { commentSchema } from "@/db/schema/comments";
+import { pinnedPostSchema } from "@/db/schema/post/pinned-post";
+import { REVALIDATE } from "@/type/constants";
+import { asc, eq, sql } from "drizzle-orm";
+import { unstable_cache } from "next/cache";
+
+const _getPinnedPosts = async <T>(): Promise<T> => {
+  console.log("캐싱안되지?");
+  const response = await db
+    .select({
+      id: pinnedPostSchema.id,
+      post_id: blogMetaSchema.post_id,
+      created_at: blogMetaSchema.created_at,
+      thumbnail_url: blogMetaSchema.thumbnail_url,
+      sub_group_name: blogSubGroup.sub_group_name,
+      like_cnt: blogMetaSchema.like_cnt,
+      post_title: blogMetaSchema.post_title,
+      post_description: blogMetaSchema.post_description,
+      order: pinnedPostSchema.order,
+      active: pinnedPostSchema.active,
+      comment_count: sql<number>`COUNT(${commentSchema.id})`.as("comment_cnt"),
+    })
+    .from(pinnedPostSchema)
+    .leftJoin(
+      blogMetaSchema,
+      eq(pinnedPostSchema.post_id, blogMetaSchema.post_id)
+    )
+    .leftJoin(
+      blogSubGroup,
+      eq(blogSubGroup.sub_group_id, blogMetaSchema.sub_group_id)
+    )
+    .leftJoin(commentSchema, eq(commentSchema.post_id, blogMetaSchema.post_id))
+    .where(eq(blogMetaSchema.status, "published"))
+    .groupBy(
+      pinnedPostSchema.id,
+      pinnedPostSchema.post_id,
+      pinnedPostSchema.order,
+      pinnedPostSchema.active,
+      blogMetaSchema.post_id,
+      blogMetaSchema.created_at,
+      blogMetaSchema.thumbnail_url,
+      blogMetaSchema.like_cnt,
+      blogMetaSchema.post_title,
+      blogMetaSchema.post_description,
+      blogMetaSchema.sub_group_id,
+      blogSubGroup.sub_group_id,
+      blogSubGroup.sub_group_name
+    )
+    .orderBy(asc(pinnedPostSchema.order));
+  return response as unknown as T;
+};
+
+const getPinnedPosts = unstable_cache(
+  _getPinnedPosts,
+  [REVALIDATE.PINNED_POST],
+  {
+    tags: [REVALIDATE.PINNED_POST],
+  }
+);
+
+export default getPinnedPosts;
